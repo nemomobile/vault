b467596133a4a39d8d75a164f48d01d3514914ea
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-05-03 09:18:22 +0300

    vault_config
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

---
+++ src/vault_config.hpp
@@ -0,0 +1,13 @@
+#ifndef _VAULT_CONFIG_HPP_
+#define _VAULT_CONFIG_HPP_
+
+namespace vault { namespace config {
+
+namespace {
+
+const QString prefix = ".f8b52b7481393a3e6ade051ecfb549fa";
+}
+
+}}
+
+#endif // _VAULT_CONFIG_HPP_
