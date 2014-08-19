6abd84208207a9d7c455ba806c302aaac483a670
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-08-19 15:13:44 +0300

    gc should be done also if there are deprecated commits
    Also options to force and dump
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- tools/git-vault-gc.sh
+++ tools/git-vault-gc.sh
@@ -5,6 +5,39 @@ function error {
     exit 1
 }
 
+ARGS=$(getopt -o fdD -- "$@");
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+force=false
+dump=false
+dump_intention=false
+
+while true; do
+    case "$1" in
+        -f)
+            force=true
+            shift;
+            ;;
+        -d)
+            dump=true
+            shift;
+            ;;
+        -D)
+            dump_intention=true
+            shift;
+            ;;
+        *)
+            if [ $# -eq 0 ]; then
+                break;
+            fi
+            error "Unknown option $1"
+            break;
+    esac
+done
+
 src=$(dirname $0)
 src=$(cd $src;pwd)
 
@@ -26,12 +59,25 @@ git log --pretty=format:%H$%d$%s \
     | tac > $cmd_file
 trap "rm $cmd_file" EXIT
 
-if ! grep '^old_tag' $cmd_file; then
-    echo "There is no need in gc for the $root"
-    exit 1
+function gen_cmd {
+    cat $cmd_file | gawk -f $src/git-vault-rebase-generate.awk
+}
+
+if ($dump || $dump_intention); then
+    $dump_intention && cat $cmd_file
+    $dump && gen_cmd
+    exit 0
 fi
 
-cmd=$(cat $cmd_file | gawk -f $src/git-vault-rebase-generate.awk)
+if ! grep '^\(old_tag\|skip\)' $cmd_file; then
+    echo "There is no need in gc for the $root"
+    if ! $force; then
+        exit 0
+    fi
+    echo "Forcing gc"
+fi
+
+cmd=$(gen_cmd)
 
 eval "$cmd" || error "$cmd"
 git reflog expire --expire=now --all || error "exiring reflog"
@@ -41,10 +87,11 @@ for obj in $(git fsck --unreachable master \
     | sed 's/unreachable blob //'); do
     for blob in $(git show $obj \
         | head -n 1 \
-        | grep '\.\./\.\./\.git/blobs' \
+        | grep '\.\./\.git/blobs' \
         | sed 's:^[./]*\.git/blobs:.git/blobs:'); do
         echo "Orphan $blob"
-        rm $blob -f || echo "Can't remove $blob"
+        (test -f $blob && (rm $blob -f || echo "Can't remove $blob")) \
+            || echo "No such blob $blob"
     done
 done
 git prune
