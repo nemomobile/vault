57306be4d0c7eb49746675777398ca54eec335ae
David Greaves <david@dgreaves.com>
2015-10-23 22:22:40 +0000

    Migration to git.merproject.org
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- tests/check_dirs_similar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $1 2>/dev/null && find -P . | sort > /tmp/.check-similar-1
-cd $2 2>/dev/null && find -P | sort > /tmp/.check-similar-2
-diff /tmp/.check-similar-1 /tmp/.check-similar-2 > /tmp/.check-similar-diff
-cat /tmp/.check-similar-diff
-
